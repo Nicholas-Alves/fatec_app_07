6cfac3c81d5f0abc3eda94b164d3f978377faaea
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-05-10 19:19:48 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ui/HomePage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
